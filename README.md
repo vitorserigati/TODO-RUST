f884d3c1fc762b369db3e85c7549d6ba64d96e79
Vitor Serigati <vitor_serigati@hotmail.com>
2023-11-16 10:48:21 -0300

    feat(persistance): created persistance for TODO

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ Uma Simples aplicação de console interativa.
 
 ## Quick Start
 ```console
-cargo run
+
+cargo run <file_name>
+```
+
+## File Format
+```txt
+TODO: alguma atividade
+DONE: atividade completa
 ```
 ## Controles
 
@@ -13,4 +20,5 @@ cargo run
 |<kbd>w</kbd>, <kbd>s</kbd>| Move para cima e para baixo|
 |<kbd>q</kbd>| Fecha o programa|
 |<kbd>Enter</kbd>| Realiza a ação do elemento selecionado na UI|
+|<kbd>f</kbd>| Persiste os dados|
 |<kbd>Tab</kbd>| Alterna entre contextos |
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use ncurses::*;
-use std::cmp::*;
+use std::cmp::min;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::{env, process};
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
@@ -56,16 +59,30 @@ impl Ui {
     }
 }
 
-enum Focus {
+#[derive(Debug)]
+enum Status {
     Todo,
     Done,
 }
-impl Focus {
+impl Status {
     fn toggle(&self) -> Self {
         match self {
-            Focus::Todo => Focus::Done,
-            Focus::Done => Focus::Todo,
+            Status::Todo => Status::Done,
+            Status::Done => Status::Todo,
         }
+    }
+}
+
+fn parse_item(line: &str) -> Option<(Status, &str)> {
+    let todo_prefix: &str = "TODO: ";
+    let done_prefix: &str = "DONE: ";
+
+    if line.starts_with("TODO: ") {
+        Some((Status::Todo, &line[todo_prefix.len()..]))
+    } else if line.starts_with(done_prefix) {
+        Some((Status::Done, &line[done_prefix.len()..]))
+    } else {
+        None
     }
 }
 
@@ -96,11 +113,42 @@ fn list_transfer(
 
 // TODO: persist the state of the application
 // TODO: add new elements to TODO
+// TODO: delete items
 // TODO: Edit the elements
 // TODO: keep track of date when the item was DONE
 // TODO: undo system
 
 fn main() {
+    let mut args = env::args();
+    args.next().unwrap();
+    let file_path = {
+        match args.next() {
+            Some(file_path) => file_path,
+            None => {
+                eprintln!("Usage: todo <file-path>");
+                eprintln!("Error: File path is not provided");
+                process::exit(1);
+            }
+        }
+    };
+    let mut todos: Vec<String> = Vec::<String>::new();
+    let mut dones: Vec<String> = Vec::<String>::new();
+    let mut quit = false;
+    let mut dones_curr: usize = 0;
+    let mut todo_curr: usize = 0;
+    let mut status = Status::Todo;
+
+    {
+        let file = File::open(file_path).unwrap();
+        for line in BufReader::new(file).lines() {
+            match parse_item(&line.unwrap()) {
+                Some((Status::Todo, line)) => todos.push(line.to_string()),
+                Some((Status::Done, line)) => dones.push(line.to_string()),
+                None => {}
+            }
+        }
+    }
+
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
@@ -111,21 +159,6 @@ fn main() {
 
     refresh();
 
-    let mut quit = false;
-    let mut todos: Vec<String> = vec![
-        "Write the todo app".to_string(),
-        "Buy a bread".to_string(),
-        "Feed the cat".to_string(),
-    ];
-    let mut dones: Vec<String> = vec![
-        "Teste".to_string(),
-        "Have a Breakfast".to_string(),
-        "Study".to_string(),
-    ];
-    let mut dones_curr: usize = 0;
-    let mut todo_curr: usize = 0;
-    let mut focus = Focus::Todo;
-
     let mut ui = Ui::default();
 
     while !quit {
@@ -133,8 +166,8 @@ fn main() {
 
         ui.begin(0, 0);
         {
-            match focus {
-                Focus::Todo => {
+            match status {
+                Status::Todo => {
                     ui.label("[TODO] DONE ", REGULAR_PAIR);
                     ui.label("------------", REGULAR_PAIR);
                     ui.begin_list(todo_curr);
@@ -143,7 +176,7 @@ fn main() {
                     }
                     ui.end_list();
                 }
-                Focus::Done => {
+                Status::Done => {
                     ui.label(" TODO [DONE]", REGULAR_PAIR);
                     ui.label("------------", REGULAR_PAIR);
                     ui.begin_list(dones_curr);
@@ -159,20 +192,29 @@ fn main() {
         let key = getch();
         match key as u8 as char {
             'q' => quit = true,
-            'w' => match focus {
-                Focus::Todo => list_up(&mut todo_curr),
-                Focus::Done => list_up(&mut dones_curr),
+            'w' => match status {
+                Status::Todo => list_up(&mut todo_curr),
+                Status::Done => list_up(&mut dones_curr),
             },
-            's' => match focus {
-                Focus::Todo => list_down(&todos, &mut todo_curr),
-                Focus::Done => list_down(&dones, &mut dones_curr),
+            's' => match status {
+                Status::Todo => list_down(&todos, &mut todo_curr),
+                Status::Done => list_down(&dones, &mut dones_curr),
             },
-            '\n' => match focus {
-                Focus::Todo => list_transfer(&mut dones, &mut todos, &mut todo_curr),
-                Focus::Done => list_transfer(&mut todos, &mut dones, &mut dones_curr),
+            '\n' => match status {
+                Status::Todo => list_transfer(&mut dones, &mut todos, &mut todo_curr),
+                Status::Done => list_transfer(&mut todos, &mut dones, &mut dones_curr),
             },
-            '\t' => focus = focus.toggle(),
-            'e' => {},
+            '\t' => status = status.toggle(),
+            'e' => {}
+            'f' => {
+                let mut file = File::create("TODO").unwrap();
+                for todo in todos.iter() {
+                    writeln!(file, "TODO: {}", todo).unwrap();
+                }
+                for done in dones.iter() {
+                    writeln!(file, "DONE: {}", done).unwrap();
+                }
+            }
 
             _ => {}
         }
